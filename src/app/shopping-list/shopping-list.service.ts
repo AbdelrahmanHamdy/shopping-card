b0f31d10a4b9c9ca73613c84feedecfca6270e8c
AbdelrahmanHamdy <abdo.hamdy@fci.helwan.edu.eg>
2019-07-31 15:07:35 +0200

    Hello world this is my first git commit message in my life ^_^

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,40 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
+
+export class ShopingListService 
+{
+
+    ingredientsChanged=new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>(); 
+    private ingredients:Ingredient[]=[
+        new Ingredient('Apples',10),
+        new Ingredient('Tomatos',99), 
+      ]; 
+
+
+    getIngredients(){
+        return this.ingredients;
+      }
+
+    getIngredient(index:number){
+        return this.ingredients[index];
+      }
+       
+    
+    addIngredient(ingredient:Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+      updateIngredient(index:number , newIngredient:Ingredient){
+      this.ingredients[index]= newIngredient;
+      this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index:number){
+    this.ingredients.splice(index,1);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+
+}
